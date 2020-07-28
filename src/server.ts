20259f55b8ffdc338fd1cbadcd31c6f23360e00a
dkdaniz <daniel@descentralizado.com.br>
2020-07-27 21:45:48 -0300

    [ADD] init

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express, { query } from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const transactions = [];
+
+app.get('/', (req, res) => {
+  const json = { message: 'hello word' };
+  res.json(json);
+})
+
+app.get('/transactions/:transactionsId', (req, res) => {
+  const { transactionsId } = req.params;
+
+  transactions.push(transactionsId);
+  res.json(transactions);
+})
+
+app.listen(3333, () => {
+  console.log('⚡️Server on port 3333');
+});
